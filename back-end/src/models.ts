32f38276d23ed69e0b6139f32de52d0a18752c74
Jacob Bastian <jcubby86@hotmail.com>
2023-04-03 20:58:55 -0600

    Refactoring

--- back-end/src/models.ts
+++ back-end/src/models.ts
@@ -12,65 +12,30 @@ const gameSchema = new Schema<IGame>(
 
 const userSchema = new Schema<IUser>({
   nickname: { type: String, required: true },
-  game: {
-    type: Schema.Types.ObjectId,
-    ref: 'Game',
-  },
+  game: { type: Schema.Types.ObjectId, ref: 'Game' },
 });
 
 const storySchema = new Schema<IStory>({
-  game: {
-    type: Schema.Types.ObjectId,
-    ref: 'Game',
-    required: true,
-  },
+  game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
+  round: { type: Number, required: true, default: 0 },
   entries: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      value: [String],
-    },
+    { user: { type: Schema.Types.ObjectId, ref: 'User' }, value: [String] },
   ],
   finalEntries: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      value: String,
-    },
+    { user: { type: Schema.Types.ObjectId, ref: 'User' }, value: String },
   ],
-  round: { type: Number, required: true, default: 0 },
 });
 
 const namesSchema = new Schema<INames>({
-  game: {
-    type: Schema.Types.ObjectId,
-    ref: 'Game',
-    required: true,
-  },
+  game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
   entries: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      value: String,
-    },
+    { user: { type: Schema.Types.ObjectId, ref: 'User' }, value: String },
   ],
 });
 
 const recreateSchema = new Schema<IRecreate>({
-  oldGame: {
-    type: Schema.Types.ObjectId,
-    ref: 'Game',
-  },
-  newGame: {
-    type: Schema.Types.ObjectId,
-    ref: 'Game',
-  },
+  oldGame: { type: Schema.Types.ObjectId, ref: 'Game' },
+  newGame: { type: Schema.Types.ObjectId, ref: 'Game' },
 });
 
 export const GameModel = model<IGame>('Game', gameSchema);
