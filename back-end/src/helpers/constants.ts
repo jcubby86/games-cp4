551778d6e18d071e59549e9608c63ff6424027e1
Jacob Bastian <jcubby86@hotmail.com>
2023-11-08 20:18:46 -0700

    add prisma

--- back-end/src/helpers/constants.ts
+++ back-end/src/helpers/constants.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 export const STORY: string = 'story';
 export const NAMES: string = 'names';
 export const END: string = 'end';
@@ -6,8 +7,8 @@ export const WAIT: string = 'wait';
 export const READ: string = 'read';
 export const PLAY: string = 'play';
 
-export const punctRegex: RegExp = /.*([.!?])$/;
-export const quoteRegex: RegExp = /["“”]/g;
+export const punctRegex = /.*([.!?])$/;
+export const quoteRegex = /["“”]/g;
 
 export const FEMALE_NAMES: string = 'femaleNames';
 export const MALE_NAMES: string = 'maleNames';
