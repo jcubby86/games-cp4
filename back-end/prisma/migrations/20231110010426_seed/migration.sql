f4cb148d339d444d9f969cf5fb20a7f9954dad1c
Jacob Bastian <jcubby86@hotmail.com>
2023-11-09 21:34:11 -0700

    fix seeding

---
+++ back-end/prisma/migrations/20231110010426_seed/migration.sql
@@ -0,0 +1,157 @@
+DELETE FROM "Suggestion";
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('PAST_ACTION','So they threw coins off the top of the space needle trying to hit people. But soon upgraded to water balloons'),
+	 ('PAST_ACTION','Got married of course! '),
+	 ('PAST_ACTION','Locked pinkies and skipped off into the sunset, singing Somewhere Over the Rainbow'),
+	 ('PAST_ACTION','Left to go look for their digits'),
+	 ('PAST_ACTION','Went gravesite shopping at the cemetery for their vacation. '),
+	 ('PAST_ACTION','Put on their big boy pants and stepped into moving traffic'),
+	 ('PAST_ACTION','So they solved world hunger, cured cancer, and explored the deepest parts of the ocean'),
+	 ('PAST_ACTION','went skydiving with the dog.'),
+	 ('PAST_ACTION','read a book with a baby'),
+	 ('PAST_ACTION','ate all of the spaghettis');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('PAST_ACTION','huffed and puffed and blew the house down'),
+	 ('PAST_ACTION','cried every time'),
+	 ('PAST_ACTION','attempted to put on each other''s straight jackets but you can''t put on someone else''s while you are wearing one.'),
+	 ('PAST_ACTION','Sold their birthrights for some stronger lungs.'),
+	 ('PAST_ACTION','Jumped on the trees and jumped on the houses and punched peoples cars '),
+	 ('PAST_ACTION','wrestled in a vat of warm milk and one of them died.'),
+	 ('PAST_ACTION','Physically entered the portal to the virtual world of the video game to save their cousin, but died from a squirtle.'),
+	 ('PAST_ACTION','Went camping in a haunted motorhome and the ghost Nate their spam'),
+	 ('PAST_ACTION','Went alligator surfing during the hurricane in florida and saved all the flamingos'),
+	 ('PAST_ACTION','So she fell into the pit of quicksand as he screamed in terror, but she only sank down to her ankles so it ended up being fine. ');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('PAST_ACTION','Retired from milking goats and started a business milking almonds '),
+	 ('PAST_ACTION','Ran away'),
+	 ('PAST_ACTION','Frolicked throughout the corn maze until they arrived at the yellow brick road'),
+	 ('PRESENT_ACTION','dancing in the rain'),
+	 ('PRESENT_ACTION','eating spaghetti'),
+	 ('PRESENT_ACTION','driving to the airport'),
+	 ('PRESENT_ACTION','sledding down the hill'),
+	 ('PRESENT_ACTION','writing love letters'),
+	 ('PRESENT_ACTION','watching Shrek 2'),
+	 ('PRESENT_ACTION','washing potatoes');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('PRESENT_ACTION','listening to white noise'),
+	 ('PRESENT_ACTION','snuggling with stuffed animals'),
+	 ('PRESENT_ACTION','reading a good book'),
+	 ('PRESENT_ACTION','coding up a fun game'),
+	 ('PRESENT_ACTION','Dancing to that song Walk like a man like ya know in that song walk like a man. '),
+	 ('PRESENT_ACTION','Bickering over who killed who'),
+	 ('PRESENT_ACTION','Being towed in a wagon while in the Running of the Bulls'),
+	 ('PRESENT_ACTION','Scuba diving in a pool of Jello '),
+	 ('PRESENT_ACTION','Hiding from a murderer in a closet while talking loudly on a phone'),
+	 ('PRESENT_ACTION','Playing mine sweeper looking to kill cute ducks');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('PRESENT_ACTION','Skipping down main street singing Christmas carols. '),
+	 ('PRESENT_ACTION','Eating their curds and whey. Gross.'),
+	 ('PRESENT_ACTION','walking on the beach and throwing the jellyfish back in the ocean'),
+	 ('PRESENT_ACTION','Selling Seashells by the homeless man in an abandoned subway tunnel'),
+	 ('PRESENT_ACTION','Looking lovingly into each others eyes. Gross.'),
+	 ('PRESENT_ACTION','separating the good water from the bad water and punishing the bad water'),
+	 ('PRESENT_ACTION','Particle colliding in their free time.'),
+	 ('PRESENT_ACTION','Watching a dcumentary about cannibalism'),
+	 ('PRESENT_ACTION','Eating ice cream at a pizza place in new york'),
+	 ('PRESENT_ACTION','Gossiping');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('FEMALE_NAME','Wynona Ryder'),
+	 ('FEMALE_NAME','The Queen of England'),
+	 ('FEMALE_NAME','Fiona'),
+	 ('FEMALE_NAME','Mary'),
+	 ('FEMALE_NAME','Elsa'),
+	 ('FEMALE_NAME','Beyonce'),
+	 ('FEMALE_NAME','Grandma'),
+	 ('FEMALE_NAME','Mom'),
+	 ('FEMALE_NAME','Honey Boo Boo'),
+	 ('FEMALE_NAME','Princess Zelda');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('FEMALE_NAME','Rashida Jones'),
+	 ('FEMALE_NAME','Leslie Knope'),
+	 ('FEMALE_NAME','Mariah Carey'),
+	 ('FEMALE_NAME','Queen Victoria'),
+	 ('FEMALE_NAME','Anastasia'),
+	 ('FEMALE_NAME','Dolores Umbridge'),
+	 ('FEMALE_NAME','Thumbelina'),
+	 ('FEMALE_NAME','Miss Piggy'),
+	 ('FEMALE_NAME','Hildegard von Bingen'),
+	 ('FEMALE_NAME','Ursula');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('FEMALE_NAME','Melania Trump'),
+	 ('FEMALE_NAME','Xena warrior princess'),
+	 ('FEMALE_NAME','Leia'),
+	 ('FEMALE_NAME','Michelle Obama'),
+	 ('FEMALE_NAME','She-hulk'),
+	 ('MALE_NAME','Jason Bourne'),
+	 ('MALE_NAME','Jeff Bezos'),
+	 ('MALE_NAME','Elon Musk'),
+	 ('MALE_NAME','Mark Zuckerburg'),
+	 ('MALE_NAME','Justin Bieber');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('MALE_NAME','Channing Tatum'),
+	 ('MALE_NAME','Robin Hood'),
+	 ('MALE_NAME','Shrek'),
+	 ('MALE_NAME','Jack Skellington'),
+	 ('MALE_NAME','Benjamin Franklin'),
+	 ('MALE_NAME','Joe Biden'),
+	 ('MALE_NAME','Chris Evans'),
+	 ('MALE_NAME','Alfred'),
+	 ('MALE_NAME','Rick Astley'),
+	 ('MALE_NAME','Grandpa');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('MALE_NAME','Dad'),
+	 ('MALE_NAME','Mario'),
+	 ('MALE_NAME','Christian Bale'),
+	 ('MALE_NAME','Judas'),
+	 ('MALE_NAME','Humphrey Higgins'),
+	 ('MALE_NAME','Frankinstein'),
+	 ('MALE_NAME','Snape'),
+	 ('MALE_NAME','Jafar'),
+	 ('STATEMENT','I like cheese'),
+	 ('STATEMENT','What did you call me?');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('STATEMENT','Why are you so mean?'),
+	 ('STATEMENT','Did you mean to call or was it an accident?'),
+	 ('STATEMENT','Look out!'),
+	 ('STATEMENT','My reflexes are just fine, look!'),
+	 ('STATEMENT','Don''t throw that away, it could be useful!'),
+	 ('STATEMENT','Wait for me!'),
+	 ('STATEMENT','Okay, but no mayonnaise this time'),
+	 ('STATEMENT','Can you grab the weekwhacker? I need to shave my back again'),
+	 ('STATEMENT','Ok let''s go change. And this time I get the sparkly pink jumpsuit!'),
+	 ('STATEMENT','I haven''t thrown a great, big tantrum in a while...');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('STATEMENT','Will you marry me? '),
+	 ('STATEMENT','Really? I thought I was the only one!'),
+	 ('STATEMENT','We need to bring back milk men.'),
+	 ('STATEMENT','I am woman hear me roar'),
+	 ('STATEMENT','I don''t read good'),
+	 ('STATEMENT','Stop breathing.  It''s annoying!'),
+	 ('STATEMENT','Oh, what a beautiful mid-morning!'),
+	 ('STATEMENT','It''s my birthday and I''ll cry if I want to, cry if I want to, cry cry cry CRY'),
+	 ('STATEMENT','I can''t hear you over my inner demons '),
+	 ('STATEMENT','McDonals forgot my McNuggets….. we are about to have a McProblem');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('STATEMENT','Boost my bottom!!!'),
+	 ('STATEMENT','I don''t think we need to bring China into this'),
+	 ('STATEMENT','I thought we were just changing a lightbulb, how did we end up here?'),
+	 ('STATEMENT','It''s hard to tap dance on the beach'),
+	 ('STATEMENT','I love it when I stroll grandly through the ocean'),
+	 ('STATEMENT','Make sure to save the wrapping paper and the bows'),
+	 ('STATEMENT','I didn''t even think the new Star Wars movies were that bad'),
+	 ('STATEMENT','Quick, let me feel!'),
+	 ('STATEMENT','New phone, who dis?'),
+	 ('STATEMENT','Stop exhaling — you''re ruining the air and making the earth toxic');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('STATEMENT','If you could be the one who if you were the greatest then if you were who you wanted to be for your parents?'),
+	 ('STATEMENT','I hate to tell you this / but I have leprosy '),
+	 ('STATEMENT','You woke me up for this?!?'),
+	 ('STATEMENT','What the frick?'),
+	 ('STATEMENT','Well I never'),
+	 ('STATEMENT','I don''t care who your mother is, I am going to get on that boat'),
+	 ('STATEMENT','That''s what she said'),
+	 ('STATEMENT','Have you seen my spleen?'),
+	 ('STATEMENT','Time to wake up to WOKE, fool.'),
+	 ('STATEMENT','Thinks for thanking of me');
+INSERT INTO "Suggestion" (category,value) VALUES
+	 ('STATEMENT','I''m not as think as you drunk I am, ocifer');
