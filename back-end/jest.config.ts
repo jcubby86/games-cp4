25980ff6f7698e71e8f6b69f2c92155b6c05c98b
Jacob Bastian <jcubby86@hotmail.com>
2023-11-11 22:33:13 -0700

    fix test config

---
+++ back-end/jest.config.ts
@@ -0,0 +1,14 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  collectCoverageFrom: ['./src/**'],
+  coveragePathIgnorePatterns: ['.generated'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  resetMocks: true,
+};
+
+export default jestConfig;
--- back-end/src/utils/types.ts
+++ back-end/src/utils/types.ts
@@ -1,5 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-namespace */
-import { RequestHandler as ExpressRequestHandler } from 'express';
+import {
+  RequestHandler as ExpressRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
 
 import { ErrorResBody, GameDto, UserDto } from '../domain/types';
 
@@ -22,8 +28,11 @@ export type ReqBody = never;
 
 export type Params = { [key: string]: string };
 
-export type ReqHandler<Req = never, Res = never> = ExpressRequestHandler<
-  Params,
-  Res | ErrorResBody,
-  Req
->;
+export interface ReqHandler<Req = never, Res = never>
+  extends ExpressRequestHandler<Params, Res | ErrorResBody, Req> {
+  (
+    req: Request<Params, Res, Req>,
+    res: Response<Res | ErrorResBody>,
+    next: NextFunction
+  ): Promise<unknown>;
+}
