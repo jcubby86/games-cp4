2441cb7f55125b207b1f1e1c0c6730701f24a8f0
Jacob Bastian <jcubby86@hotmail.com>
2023-11-18 23:24:24 -0700

    add more tests to integration/games.test.ts

--- back-end/test/integration/games.test.ts
+++ back-end/test/integration/games.test.ts
@@ -1,10 +1,20 @@
-import { describe, expect, test } from '@jest/globals';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { beforeAll, describe, expect, test } from '@jest/globals';
 import request from 'supertest';
 
 import app from '../../src/server';
 
+const context: any = {};
+
+beforeAll(async () => {
+  const createResponse = await request(app)
+    .post('/api/game')
+    .send({ type: 'name' });
+  context.game = createResponse.body;
+});
+
 describe('createGame', () => {
-  test('should get OK', async () => {
+  test('success', async () => {
     const res = await request(app).post('/api/game').send({ type: 'story' });
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty('code');
@@ -17,17 +27,50 @@ describe('createGame', () => {
 });
 
 describe('updateGame', () => {
-  test('should get OK', async () => {
-    const createResponse = await request(app)
-      .post('/api/game')
-      .send({ type: 'name' });
-    expect(createResponse.body.phase).toMatch('JOIN');
+  test('not found', async () => {
+    await request(app).put(`/api/game/CODE`).send({ phase: 'end' }).expect(404);
+  });
 
+  test('success', async () => {
     const updateResponse = await request(app)
-      .put(`/api/game/${createResponse.body.uuid}`)
+      .put(`/api/game/${context.game.uuid}`)
       .send({ phase: 'end' });
     expect(updateResponse.statusCode).toBe(200);
-    expect(updateResponse.body.uuid).toMatch(createResponse.body.uuid);
+    expect(updateResponse.body.uuid).toMatch(context.game.uuid);
     expect(updateResponse.body.phase).toMatch('END');
+  });
+});
+
+describe('getGame', () => {
+  test('not found', async () => {
+    await request(app).get('/api/game/CODE').expect(404);
+  });
+
+  test('success', async () => {
+    const getResponse = await request(app).get(
+      `/api/game/${context.game.code}`
+    );
+    expect(getResponse.statusCode).toBe(200);
+    expect(getResponse.body.uuid).toMatch(context.game.uuid);
+  });
+});
+
+describe('recreateGame', () => {
+  test('not found', async () => {
+    await request(app).post('/api/game/UUID/recreate').expect(404);
+  });
+
+  test('success', async () => {
+    const response1 = await request(app).post(
+      `/api/game/${context.game.uuid}/recreate`
+    );
+    expect(response1.statusCode).toBe(200);
+
+    const response2 = await request(app).post(
+      `/api/game/${context.game.uuid}/recreate`
+    );
+    expect(response2.statusCode).toBe(200);
+
+    expect(response1.body).toMatchObject(response2.body);
   });
 });
